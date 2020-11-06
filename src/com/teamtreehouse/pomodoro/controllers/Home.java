7c37936dbfcbdd1fb49f366455be6c9869365c78
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-06 08:24:15 +0000

    Create a property and bind it to a controller. This property will send events out to all its listeners when changed

--- src/com/teamtreehouse/pomodoro/controllers/Home.java
+++ src/com/teamtreehouse/pomodoro/controllers/Home.java
@@ -2,6 +2,8 @@ package com.teamtreehouse.pomodoro.controllers;
 
 import com.teamtreehouse.pomodoro.model.Attempt;
 import com.teamtreehouse.pomodoro.model.AttemptKind;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -13,12 +15,37 @@ public class Home {
     @FXML private Label title;
 
     private Attempt currentAttempt;
+    private StringProperty timerText;
+
+    public Home() {
+        timerText = new SimpleStringProperty();
+        setTimerText(0);
+    }
+
+    public String getTimerText() {
+        return timerText.get();
+    }
+
+    public StringProperty timerTextProperty() {
+        return timerText;
+    }
+
+    public void setTimerText(String timerText) {
+        this.timerText.set(timerText);
+    }
+
+    public void setTimerText(int remainingSeconds) {
+        int minutes = remainingSeconds/60;
+        int seconds = remainingSeconds%60;
+        setTimerText(String.format("%02d:%02d", minutes, seconds));
+    }
 
     private void prepareAttempt(AttemptKind kind) {
         clearAttemptStyles();
         currentAttempt = new Attempt(kind, "");
         addAttemptStyle(kind);
         title.setText(kind.getDisplayName());
+        setTimerText(currentAttempt.getRemainingSeconds());
     }
 
     private void addAttemptStyle(AttemptKind kind) {
